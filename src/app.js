f1ad72c7f04a1a7b5cb9495fefab70ec2627587e
cyberplanner <sal@salweb.co.uk>
2017-04-11 16:13:26 +0100

    :wrench: Add widthScale and refactor code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-var canvas =  d3.select("body")
-                .append("svg")
-                .attr("width", 500)
-                .attr("height", 500);
+var dataArray   =  [4, 10, 50, 13, 20, 60];
+var width       = 500;
+var height      = 500;
+
+var widthScale  = d3.scaleLinear()
+                      .domain([0, 60])
+                      .range([0, width]);
+
+
+var canvas      =  d3.select("body")
+                      .append("svg")
+                      .attr("width", width)
+                      .attr("height", height);
+
+var bars        =  canvas.selectAll("rect")
+                        .data(dataArray)
+                        .enter()
+                          .append("rect")
+                          .attr("width", function(d) { return widthScale(d);})
+                          .attr("height", 30)
+                          .attr("y", function(d, i) { return i * 50; });
+
+
+
 
 // var testCircle =  canvas.append("circle")
 //                         .attr("cy", 250)
